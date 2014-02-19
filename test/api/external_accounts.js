2c64ae1937ac2d8b475a6946948abba3f3480cb5
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-19 11:47:15 -0800

    [TEST] Add external_accounts api CRUD tests.

--- test/api/external_accounts.js
+++ test/api/external_accounts.js
@@ -1,24 +1,106 @@
 var assert = require('assert');
+var adapter = require('../../adapters/test_adapter');
+var uuid = require('node-uuid');
 
 describe('External Accounts', function() {
-  it('should require a name', function(fn){
+  beforeEach(function(){
+    opts = {};
+  });
+  
+  describe('creating an external account', function(){
+    it('should require a name', function(fn){
+      adapter.createExternalAccount(opts, function(err, external_account){
+        assert(err.name);
+        assert(!external_account);
+      }); 
+    });
+
+    it('should not permit a redundant name/user id pair', function(fn){
+      opts = {
+        user_id: 1,
+        name: uuid.v1()
+      }
+      adapter.createExternalAccount(opts, function(err, external_account){
+        assert(external_account.name == opts.name);
+        adapter.createExternalAccount(opts, function(err, external_account){
+          assert(err.user_id);
+          assert(err.name);
+          assert(!external_account);
+        });
+      });
+    });
+  });
+
+  describe('reading an external account', function(){
+    it('should retrieve an external account record', function(fn){
+      opts = {
+        user_id: 1,
+        name: uuid.v1()
+      }
+      adapter.createExternalAccount(opts, function(err, external_account){
+        opts = { id: external_account.id };
+        adapter.getExternalAcccout(opts, function(err, external_account){
+          assert(external_account.id, opts.id);
+          assert(!err);
+        });
+      });
+    });
 
   });
 
-  it('should require a user id', function(fn){
+  describe('updating an external account', function(){
+    it('should update user id', function(fn){
+      opts = {
+        user_id: 1,
+        name: uuid.v1()
+      }
+      adapter.createExternalAccount(opts, function(err, external_account){
+        opts.user_id = 2;
+        adapter.updateExternalAccount(opts, function(err, external_account){
+          assert(!err);
+          assert(external_account.user_id == 2);
+        });
+      });
+    });
+
+    it('should update the name of an account', function(fn){
+      opts = {
+        user_id: 1,
+        name: uuid.v1()
+      }
+      adapter.createExternalAccount(opts, function(err, external_account){
+        assert(external_account.name == opts.name);
+        opts.name = uuid.v1();
+        adapter.updateExternalAccount(opts, function(err, external_account){
+          assert(!err);
+          assert(external_account.name == opts.name);
+        });
+      });
+
+    });
 
   });
 
-  it('should not permit a redundant name/user id pair', function(fn){
-
-  });
-
-  it('should update user id and name', function(fn){
-
-  });
-
-  it('should delete an external account', function(fn){
-
+  describe('deleting an external account', function(fn){
+    it('should delete an external account', function(fn){
+      opts = {
+        user_id: 1,
+        name: uuid.v1()
+      }
+      adapter.createExternalAccount(opts, function(err, external_account){
+        opts = {
+          id: external_account.id
+        };
+        adapter.destroyExternalAccount(opts, function(err, external_account){
+          assert(!err);
+          assert(external_account.name == opts.name);
+          adapter.getExternalAccount(opts, function(err, external_account){
+            assert(err.id);
+            assert(!external_account);
+          });
+        });
+      });
+    });
   });
 
 });
