ac75b2f32f27338bef36c21630af5c9c9c5cc20b
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-27 22:03:23 -0800

    [TEST] Add test for users api.

--- test/api/users.js
+++ test/api/users.js
@@ -1,5 +1,8 @@
 var Adapter = require(process.env.GATEWAY_DATA_ADAPTER || '../../adapters/test_adapter');
 var assert = require('assert');
+var crypto = require('crypto');
+
+function random() { return crypto.randomBytes(16).toString('hex') };
 
 describe('Users', function(){
 
@@ -7,7 +10,88 @@ describe('Users', function(){
 
   before(function(){
     adapter = new Adapter();
+    opts = {};
   });
+
+  describe('creating a user', function(){
+
+    it('should succeed with a username and password', function(fn){
+    
+      opts = {
+        name: random(),
+        password: random()
+      } 
+      adapter.createUser(opts, function(err, user){
+        assert(!err);
+        assert(user.id > 0);
+        assert(user.name == opts.name); 
+        fn();
+      });
+    });
+
+    it('should store a unique salt for every user', function(fn){
+      opts = {
+        name: random(), 
+        password: random()
+      }; 
+      adapter.createUser(opts, function(err, user){
+        assert(user.salt);
+        var firstSalt = user.salt;
+        opts = {
+          name: random(), 
+          password: random()
+        };
+        adapter.createUser(opts, function(err, user){
+          assert(user.salt);
+          assert(firstSalt != user.salt);
+          fn();
+        });
+      });
+    });
+
+    it('should hash every password with the corresponding salt', function(fn) {
+      opts = {
+        name: random(), 
+        password: random(),
+      } 
+      adapter.createUser(opts, function(err, user){
+        assert(!err);
+        assert(!user.password);
+        assert(user.password_hash)
+        fn();
+      });
+
+    });
+  
+    it('should not be able to create two users with the same name', function(fn){
+      opts = {
+        name: random(), 
+        password: random()
+      } 
+      adapter.createUser(opts, function(err, user){
+        adapter.createUser(opts, function(err, user){
+          assert(err.name);
+          assert(!user);
+          fn();
+        }); 
+      }); 
+    });
+
+    it('should not be able to create a user named "admin"', function(fn){
+      opts = {
+        name: 'admin',
+        password: random()
+      } 
+      adapter.createUser(opts, function(err, user){
+        assert(err.name);
+        assert(!user);
+        fn();
+      });
+
+    }); 
+
+  });
+
 
   it.skip('should allow storage of arbitrary "data" key-value store.', function(done){
 
